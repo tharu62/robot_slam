deeb0623ec3f0aab69fcd883f5b1db16c3880a75
tharu62 <tharinduxdeshan@gmail.com>
2025-03-03 17:12:32 +0100

    adds urdf

--- launch/talker.launch.py
+++ launch/talker.launch.py
@@ -1,13 +1,28 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
 def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            package='robot_slam',
-            executable='talker',
-            name='talker',
-            output='screen',
-            parameters=[{'use_sim_time': True}]
-        )
-    ])
+    ld = LaunchDescription()
+
+    talker_node = Node(
+        package='robot_slam',
+        executable='talker_lidar',
+        name='talker_lidar',
+        output='screen',
+        parameters=[{'use_sim_time': True}]
+    )
+
+    ld.add_action(talker_node)
+    
+    ld.add_action(IncludeLaunchDescription(
+        PathJoinSubstitution([FindPackageShare('urdf_launch'), 'launch', 'display.launch.py']),
+        launch_arguments={
+            'urdf_package': 'description',
+            'urdf_package_path': PathJoinSubstitution(['urdf', 'robot.urdf'])}.items()
+    ))
+    
+    return ld
