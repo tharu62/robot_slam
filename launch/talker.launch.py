b879c26d6336d24592be44ddc2e71e5fbc48e201
Yedi278 <Yehan278@gmail.com>
2025-03-03 22:59:25 +0100

    lidar test 1

--- launch/talker.launch.py
+++ launch/talker.launch.py
@@ -6,46 +6,47 @@ from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
-    ld = LaunchDescription()
+        ld = LaunchDescription()
 
-    urdf_project_path = FindPackageShare('robot_slam')
-    # urdf_tutorial_path = FindPackageShare('urdf_tutorial')
-    default_model_path = PathJoinSubstitution([urdf_project_path, 'description', 'robot.urdf.xacro'])
-    default_rviz_config_path = PathJoinSubstitution([urdf_project_path, 'rviz', 'urdf.rviz'])
+        urdf_project_path = FindPackageShare('robot_slam')
+        # urdf_tutorial_path = FindPackageShare('urdf_tutorial')
+        default_model_path = PathJoinSubstitution([urdf_project_path, 'description', 'robot.urdf.xacro'])
+        default_rviz_config_path = PathJoinSubstitution([urdf_project_path, 'rviz', 'urdf.rviz'])
 
-    talker_node = Node(
-        package='robot_slam',
-        executable='talker_lidar',
-        name='talker_lidar',
-        output='screen',
-        parameters=[{'use_sim_time': True}]
-    )
+        # Talk Lidar Node (avvia il publisher del lidar)
+        talker_node = Node(
+                package='robot_slam',
+                executable='talker_lidar',
+                name='talker_lidar',
+                output='screen',
+                parameters=[{'use_sim_time': True}]
+        )
 
-    # These parameters are maintained for backwards compatibility
-    gui_arg = DeclareLaunchArgument(name='gui', default_value='true', choices=['true', 'false'],
-            description='Flag to enable joint_state_publisher_gui')
-    ld.add_action(gui_arg)
-    rviz_arg = DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
-            description='Absolute path to rviz config file')
-    ld.add_action(rviz_arg)
+        # These parameters are maintained for backwards compatibility
+        gui_arg = DeclareLaunchArgument(name='gui', default_value='true', choices=['true', 'false'],
+                description='Flag to enable joint_state_publisher_gui')
+        ld.add_action(gui_arg)
+        rviz_arg = DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
+                description='Absolute path to rviz config file')
+        ld.add_action(rviz_arg)
 
-    # This parameter has changed its meaning slightly from previous versions
-    ld.add_action(DeclareLaunchArgument(name='model', default_value=str(default_model_path),
-            description='Path to robot urdf file relative to robot_slam package'))
+        # This parameter has changed its meaning slightly from previous versions
+        ld.add_action(DeclareLaunchArgument(name='model', default_value=str(default_model_path),
+                description='Path to robot urdf file relative to robot_slam package'))
 
-    ld.add_action(IncludeLaunchDescription(
-        PathJoinSubstitution([FindPackageShare('urdf_launch'), 'launch', 'display.launch.py']),
-        launch_arguments={
-            'urdf_package': 'robot_slam',
-            'urdf_package_path': LaunchConfiguration('model'),
-            'rviz_config': LaunchConfiguration('rvizconfig'),
-            'jsp_gui': LaunchConfiguration('gui')}.items()
-    ))
+        ld.add_action(IncludeLaunchDescription(
+                PathJoinSubstitution([FindPackageShare('urdf_launch'), 'launch', 'display.launch.py']),
+                launch_arguments={
+                'urdf_package': 'robot_slam',
+                'urdf_package_path': LaunchConfiguration('model'),
+                'rviz_config': LaunchConfiguration('rvizconfig'),
+                'jsp_gui': LaunchConfiguration('gui')}.items()
+        ))
 
-    ld.add_action(talker_node)
+        ld.add_action(talker_node)
 
-    # cmd per lanciare launch file con rviz:
-    # ros2 launch robot_slam talker.launch.py model:=/home/utonto/ros2_ws/src/robot_slam/description/robot.urdf.xacro
-    # ros2 run joint_state_publisher_gui joint_state_publisher_gui
-    
-    return ld
+        # cmd per lanciare launch file con rviz:
+        # ros2 launch robot_slam talker.launch.py model:=/home/utonto/ros2_ws/src/robot_slam/description/robot.urdf.xacro
+        # ros2 run joint_state_publisher_gui joint_state_publisher_gui
+        
+        return ld
--- src/lidar_publisher.hpp
+++ src/lidar_publisher.hpp
@@ -1,6 +1,8 @@
 #ifndef LIDAR_PUBLISHER_HPP
 #define LIDAR_PUBLISHER_HPP
 
+#define RANGES_BUFF_SIZE 100
+
 #include <chrono>
 #include <memory>
 #include <string>
@@ -26,90 +28,99 @@ struct Data{
   float distance;
 };
 
-extern Data last_read_data;
-extern double last_data_time;
-extern float last_data_angle;
+// double last_data_time=0;
+// float last_data_angle=0;
 extern int client;
 
 class Lidar_Publisher : public rclcpp::Node
 {
+  private:
+  double first_time = 0.0;
+  double last_time = 0.0;
+  float first_angle = 0.0;
+  float last_angle = 0.0;
+  float rpm_ = 0.0;
+  float distance_[RANGES_BUFF_SIZE];
+
   public:
   sensor_msgs::msg::LaserScan msg_scan = sensor_msgs::msg::LaserScan();
   char buffer[TCP_BUFFSIZE];
   char temp;
-  double time_;
   std::string input_data;
   std::string str;
   json output_data;
 
   Lidar_Publisher()
-    : Node("minimal_publisher"), count_(0)
+    : Node("minimal_publisher"), distance_{0.0}
     {
       publisher_ = this->create_publisher<sensor_msgs::msg::LaserScan>("laser_scan", 100);
-      timer_ = this->create_wall_timer( 10ms, [this]()->void{ this->call_back(); });
+      timer_ = this->create_wall_timer( 1ms, [this]()->void{ this->call_back(); });
     }
 
   void call_back(){
-    
-    input_data = "";
-    for(int i = 0; i < TCP_BUFFSIZE; i++){
-      buffer[i] = '\0';
-    }
-    //std::cout << "Received: ";
-    for(int i = 0; temp != '\n'; i++){
-      recv(client, &temp, 1, 0);
-      if(temp == '\n'){
-        break;
-      }
-      buffer[i] = temp;
-    //std::cout << buffer[i];
-    }
-    //std::cout << std::endl;
-    temp = '\0';
-  
-    if(buffer[0] != '\0'){
 
-      input_data = buffer;
+    for(int i=0; i<RANGES_BUFF_SIZE; ++i){
+      distance_[i] = 0.0;
       
+      input_data = "";
       for(int i = 0; i < TCP_BUFFSIZE; i++){
         buffer[i] = '\0';
       }
-      
-      json j = json::parse(input_data);
-      last_read_data.angle = j["angle"].get<float>() * 3.14/180.0;
-      last_read_data.rpm = j["rpm"].get<float>();
-      last_read_data.distance = j["dist"].get<float>();
-      
-      msg_scan.header.frame_id = "laser_frame";
-      msg_scan.header.stamp = this->now();
+      for(int i = 0; temp != '\n'; i++){
+        recv(client, &temp, 1, 0);
+        if(temp == '\n'){
+          break;
+        }
+        buffer[i] = temp;
+      }
+      temp = '\0';
+    
+      if(buffer[0] != '\0'){
 
-      msg_scan.angle_min = last_read_data.angle;
-      msg_scan.angle_max = last_data_angle; 
+        input_data = buffer;
+        
+        for(int i = 0; i < TCP_BUFFSIZE; i++){
+          buffer[i] = '\0';
+        }
 
-      // msg_scan.angle_increment = last_read_data.angle - last_data_angle;
-      msg_scan.angle_increment = 1.1f;
+        json j = json::parse(input_data);
+        
+        if(i == 0){
+          first_angle = j["angle"].get<float>() * M_PI/180.0;
+          first_time = this->now().seconds();
+        }
+        if(i == RANGES_BUFF_SIZE-1){
+          last_angle = j["angle"].get<float>() * M_PI/180.0;
+          last_time = this->now().seconds();
+        }
 
-      last_data_angle = last_read_data.angle;
-      time_ = this->now().seconds();
+        // last_read_data.angle = j["angle"].get<float>() * 3.14/180.0;
+        rpm_ = j["rpm"].get<float>();
 
-      msg_scan.time_increment = 0.0;
+        distance_[i] = j["dist"].get<float>() / 1000.0;
 
-      last_data_time = time_;
-      msg_scan.scan_time = 0.1;
-
-      msg_scan.range_min = 0.001f;
-      msg_scan.range_max = 10.0f;
-      msg_scan.ranges = {1.0f, 1.1f};
-      msg_scan.intensities = {1.0f, 1.0f};
-
-      this->publisher_->publish(msg_scan);
+      }else --i;
     }
+
+    msg_scan.header.frame_id = "laser_frame";
+    msg_scan.header.stamp = this->now();
+    msg_scan.angle_min = first_angle;
+    msg_scan.angle_max = last_angle; 
+    msg_scan.angle_increment = M_PI / 180.0;
+    // msg_scan.angle_increment = (last_angle - first_angle) / (float)RANGES_BUFF_SIZE;
+    msg_scan.time_increment = (float)(last_time - first_time) / (float)RANGES_BUFF_SIZE;
+    msg_scan.scan_time = (float)(last_time - first_time);
+    msg_scan.range_min = 0.05;
+    msg_scan.range_max = 10.00;
+    msg_scan.ranges.assign(std::begin(distance_), std::end(distance_));
+    msg_scan.intensities.assign(std::begin(distance_), std::end(distance_));
+
+    this->publisher_->publish(msg_scan);
   }
       
   private:
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<sensor_msgs::msg::LaserScan>::SharedPtr publisher_;
-    size_t count_;
 };
 
 #endif // LIDAR_PUBLISHER_HPP
