facce155eeab5dc01fcd5d9150e236f5c17591b1
tharu62 <tharinduxdeshan@gmail.com>
2025-03-03 18:43:03 +0100

    setup xacro and launch file

--- launch/talker.launch.py
+++ launch/talker.launch.py
@@ -1,12 +1,17 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
-from launch.actions import IncludeLaunchDescription
-from launch.substitutions import PathJoinSubstitution
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
     ld = LaunchDescription()
+
+    urdf_project_path = FindPackageShare('robot_slam')
+    # urdf_tutorial_path = FindPackageShare('urdf_tutorial')
+    default_model_path = PathJoinSubstitution([urdf_project_path, 'description', 'robot.urdf.xacro'])
+    default_rviz_config_path = PathJoinSubstitution([urdf_project_path, 'rviz', 'urdf.rviz'])
 
     talker_node = Node(
         package='robot_slam',
@@ -16,13 +21,30 @@ def generate_launch_description():
         parameters=[{'use_sim_time': True}]
     )
 
-    ld.add_action(talker_node)
-    
+    # These parameters are maintained for backwards compatibility
+    gui_arg = DeclareLaunchArgument(name='gui', default_value='true', choices=['true', 'false'],
+            description='Flag to enable joint_state_publisher_gui')
+    ld.add_action(gui_arg)
+    rviz_arg = DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
+            description='Absolute path to rviz config file')
+    ld.add_action(rviz_arg)
+
+    # This parameter has changed its meaning slightly from previous versions
+    ld.add_action(DeclareLaunchArgument(name='model', default_value=str(default_model_path),
+            description='Path to robot urdf file relative to urdf_tutorial package'))
+
     ld.add_action(IncludeLaunchDescription(
         PathJoinSubstitution([FindPackageShare('urdf_launch'), 'launch', 'display.launch.py']),
         launch_arguments={
-            'urdf_package': 'description',
-            'urdf_package_path': PathJoinSubstitution(['urdf', 'robot.urdf'])}.items()
+            'urdf_package': 'urdf_tutorial',
+            'urdf_package_path': LaunchConfiguration('model'),
+            'rviz_config': LaunchConfiguration('rvizconfig'),
+            'jsp_gui': LaunchConfiguration('gui')}.items()
     ))
-    
+
+    # ld.add_action(talker_node)
+
+    # cmd per lanciare launch file con rviz:
+    # ros2 launch robot_slam talker.launch.py model:=/home/utonto/ros2_ws/src/robot_slam/description/robot.urdf.xacro
+
     return ld
