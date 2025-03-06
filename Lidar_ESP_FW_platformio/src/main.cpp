e42ba175fa70bf253ba20cb3bd6d7d2075fc0ee3
Yedi278 <Yehan278@gmail.com>
2025-03-06 17:08:18 +0100

    udp

--- Lidar_ESP_FW_platformio/src/main.cpp
+++ Lidar_ESP_FW_platformio/src/main.cpp
@@ -3,7 +3,7 @@
 #define RXPIN 3
 #define TXPIN 18
 #define LIDAR_BAUDRATE 115200 //transmission speed for lidar
-#define BAUDRATE 300000 //transmission speed for pc serial connection
+#define BAUDRATE 460800 //transmission speed for pc serial connection
 
 // specifications for data transmission
 // DON'T CHANGE THE VALUES
@@ -14,8 +14,8 @@
 #define PACKET_SIZE 90
 
 // choose transmission type
-#define TRANSMISSION_TYPE SERIAL_ONLY
-// #define TRANSMISSION_TYPE WIFI_ONLY
+// #define TRANSMISSION_TYPE SERIAL_ONLY
+#define TRANSMISSION_TYPE WIFI_ONLY
 // #define TRANSMISSION_TYPE WIFI_AND_SERIAL
 
 #include <Arduino.h>
@@ -24,13 +24,18 @@
 #include <fstream>
 #include <string.h>
 #include <WiFi.h>
+#include <WiFiUdp.h>
 #include "lidar.h"
+
+#include <chrono>
 
 
 // USART 1
 HardwareSerial Lidar(1);
-WiFiServer server(WIFI_PORT);
-WiFiClient client;
+
+// UDP server
+WiFiUDP udp;
+
 int status = WL_IDLE_STATUS;
 uint8_t buffer[30] = {0};
 uint8_t raw_data=0;
@@ -39,12 +44,10 @@ uint8_t raw_data_old=0;
 int count = 0;
 JsonDocument doc;
 
-JsonArray ang = doc["ang"].to<JsonArray>();
-JsonArray d1 = doc["data"].to<JsonArray>();
-
-
 int packet_counter = 0;
 int packet[PACKET_SIZE][5]= {0};
+uint16_t remote_port = 80;
+IPAddress remote_ip;
 
 extern const char* ssid;
 extern const char* password;
@@ -59,7 +62,6 @@ void setup(){
   Serial.println("Connecting to Wifi...");
   // Serial.printf("SSID: %s\n",ssid);
   // Serial.printf("Password: %s\n",password);
-  
   WiFi.begin(ssid, password);                         
   Serial.println("Establishing connection to WiFi with SSID: " + *(ssid));    
   while (WiFi.status() != WL_CONNECTED) {             
@@ -69,89 +71,79 @@ void setup(){
   Serial.print("Connected to network with IP address: ");
   Serial.println(WiFi.localIP());
 
-  server.begin();
+  // UDP server
+  udp.begin(WIFI_PORT);
+  
+  while(udp.available() == 0){
+    udp.parsePacket();
+    delay(100);
+  }
+  remote_ip = udp.remoteIP();
+  remote_port = udp.remotePort();
+
+  Serial.print("Remote IP: ");
+  Serial.println(remote_ip.toString());
+  Serial.printf("Remote Port: %d\n", remote_port);
+
   #endif
 
   // ensure TXPIN is only for receiving data
   pinMode(TXPIN, INPUT);
   Lidar.begin(LIDAR_BAUDRATE, SERIAL_8N1, RXPIN, TXPIN);
 
-  #if TRANSMISSION_TYPE == WIFI_ONLY || TRANSMISSION_TYPE == WIFI_AND_SERIAL
-  // client connection
-  Serial.println("Waiting for client connection...");
-  client = server.available();
-  while (!client) {
-    client = server.available();
-  }
-  Serial.println("Client connected");  
-  #endif
 
 }
+
+
+void loop(){
+  
+  udp.beginPacket(remote_ip, remote_port);
+
+  udp.write('a');
+
+  udp.endPacket();
+}
+
+#if 0
 
 void loop() {
 
-  #if TRANSMISSION_TYPE == WIFI_ONLY || TRANSMISSION_TYPE == WIFI_AND_SERIAL
-  if(client){
-  #endif
-  
-    if(Lidar.read(&temp, 1)){
-      
-      raw_data_old = raw_data;
-      raw_data = temp;
-      
-      // if end of packet is reached
-      if(raw_data_old == 0XFA && raw_data <= 0XF9 && raw_data >= 0XA0){
-
-        ang.add(angle(buffer));
-        d1.add(dist_mm(buffer+4));
-        d1.add(dist_mm(buffer+8));
-        d1.add(dist_mm(buffer+12));
-        d1.add(dist_mm(buffer+16));
-
-        packet_counter++;
-        
-        if(packet_counter == PACKET_SIZE){
-          
-          #if TRANSMISSION_TYPE == SERIAL_ONLY || TRANSMISSION_TYPE == WIFI_AND_SERIAL
-          serializeJson(doc, Serial);
-          Serial.println();
-          #endif
-          #if TRANSMISSION_TYPE == WIFI_ONLY || TRANSMISSION_TYPE == WIFI_AND_SERIAL
-          serializeJson(doc, client);
-          client.println();
-          #endif
-          
-          ang.clear();
-          d1.clear();
-          packet_counter = 0;
-        }
-
-        if(raw_data == 0xA0){
-
-          buffer[0] = 0XFA;
-          buffer[1] = raw_data;
-          count = 2;
-          packet_counter = 0;
-          
-          return;
-        }
-
-        buffer[0] = 0XFA;
-        buffer[1] = raw_data;
-
-        count = 2;
-        return;
-      }
-
-      if(count <= 22){
-        buffer[count] = raw_data;
-        count++;
-      }
-    }
+  if(Lidar.read(&temp, 1)){
     
-  #if TRANSMISSION_TYPE == WIFI_ONLY || TRANSMISSION_TYPE == WIFI_AND_SERIAL
-  }else{
-    client = server.available();
+    raw_data_old = raw_data;
+    raw_data = temp;
+    
+    // if end of packet is reached
+    if(raw_data_old == 0XFA && raw_data <= 0XF9 && raw_data >= 0XA0){
+
+      doc["ang"] = angle(buffer);
+      doc["d1"] = dist_mm(buffer+4);
+      doc["d2"] = dist_mm(buffer+8);
+      doc["d3"] = dist_mm(buffer+12);
+      doc["d4"] = dist_mm(buffer+16);
+
+      #if TRANSMISSION_TYPE == SERIAL_ONLY || TRANSMISSION_TYPE == WIFI_AND_SERIAL
+      serializeJson(doc, Serial);
+      Serial.println();
+      #endif
+      #if TRANSMISSION_TYPE == WIFI_ONLY || TRANSMISSION_TYPE == WIFI_AND_SERIAL
+      serializeJson(doc, server);
+      server.println();
+      #endif
+
+      buffer[0] = 0XFA;
+      buffer[1] = raw_data;
+
+      count = 2;
+      return;
+    }
+
+    if(count <= 22){
+      buffer[count] = raw_data;
+      count++;
+    }
   }
-  #endif
+
 }
+
+#endif
