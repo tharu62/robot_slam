5a2719731345c3316e2d7a6b69744c2547f96b1d
Yedi278 <Yehan278@gmail.com>
2025-03-06 01:21:09 +0100

    Lidar packet reading changed

--- Lidar_ESP_FW_platformio/src/main.cpp
+++ Lidar_ESP_FW_platformio/src/main.cpp
@@ -11,10 +11,12 @@
 #define WIFI_ONLY 1
 #define WIFI_AND_SERIAL 2
 
+#define PACKET_SIZE 90
+
 // choose transmission type
-#define TRANSMISSION_TYPE SERIAL_ONLY
+// #define TRANSMISSION_TYPE SERIAL_ONLY
 // #define TRANSMISSION_TYPE WIFI_ONLY
-// #define TRANSMISSION_TYPE WIFI_AND_SERIAL
+#define TRANSMISSION_TYPE WIFI_AND_SERIAL
 
 #include <Arduino.h>
 #include <HardwareSerial.h>
@@ -37,8 +39,12 @@ uint8_t raw_data_old=0;
 int count = 0;
 JsonDocument doc;
 
+// JsonArray ang = doc["ang"].to<JsonArray>();
+JsonArray d1 = doc["data"].to<JsonArray>();
+
+
 int packet_counter = 0;
-int packet[360][5] = {0};
+int packet[PACKET_SIZE][5]= {0};
 
 extern const char* ssid;
 extern const char* password;
@@ -46,14 +52,13 @@ extern const char* password;
 void setup(){
   // initialise serial comunication
   Serial.begin(BAUDRATE);
-  
   Serial.println("");
 
   #if TRANSMISSION_TYPE == WIFI_ONLY || TRANSMISSION_TYPE == WIFI_AND_SERIAL
   // initialise wifi connection
   Serial.println("Connecting to Wifi...");
-  Serial.printf("SSID: %s\n",ssid);
-  Serial.printf("Password: %s\n",password);
+  // Serial.printf("SSID: %s\n",ssid);
+  // Serial.printf("Password: %s\n",password);
   
   WiFi.begin(ssid, password);                         
   Serial.println("Establishing connection to WiFi with SSID: " + *(ssid));    
@@ -90,76 +95,59 @@ void loop() {
   #endif
   
     if(Lidar.read(&temp, 1)){
-    
-        raw_data_old = raw_data;
-        raw_data = temp;
+      
+      raw_data_old = raw_data;
+      raw_data = temp;
+      
+      // if end of packet is reached
+      if(raw_data_old == 0XFA && raw_data <= 0XF9 && raw_data >= 0XA0){
+
+        // ang.add(angle(buffer));
+        d1.add(dist_mm(buffer+4));
+        // d1.add(dist_mm(buffer+8));
+        // d1.add(dist_mm(buffer+12));
+        // d1.add(dist_mm(buffer+16));
+
+        packet_counter++;
         
-        // if end of packet is reached
-        if(raw_data_old == 0XFA && raw_data <= 0XF9 && raw_data >= 0XA0){
-
-          if(packet_counter == 360){
-            for(int i=0; i<360; i++){
-              doc["ang"] = packet[i][0];
-              doc["d1"] = packet[i][1];
-              doc["d2"] = packet[i][2];
-              doc["d3"] = packet[i][3];
-              doc["d4"] = packet[i][4];
-
-              #if TRANSMISSION_TYPE == SERIAL_ONLY
-                serializeJson(doc, Serial);
-                Serial.println();
-              #elif TRANSMISSION_TYPE == WIFI_ONLY
-                serializeJson(doc, client); 
-                client.println();
-              #elif TRANSMISSION_TYPE == WIFI_AND_SERIAL
-                serializeJson(doc, Serial);
-                Serial.println();
-                serializeJson(doc, client);
-                client.println();
-              #endif
-            }
-            packet_counter = 0;
-          }
-
-          packet[packet_counter][0] = angle(buffer);
-          packet[packet_counter][1] = dist_mm(buffer);
-          packet[packet_counter][2] = dist_mm(buffer+4);
-          packet[packet_counter][3] = dist_mm(buffer+8);
-          packet[packet_counter][4] = dist_mm(buffer+12);
-          packet_counter++;
-
-          // print_data(buffer);
+        if(packet_counter == PACKET_SIZE){
           
-          // doc["ang"] = angle(buffer);
-          // doc["d1"] = dist_mm(buffer);
-          // doc["d2"] = dist_mm(buffer+4);
-          // doc["d3"] = dist_mm(buffer+8);
-          // doc["d4"] = dist_mm(buffer+12);
-    
-          // #if TRANSMISSION_TYPE == SERIAL_ONLY
-          //   serializeJson(doc, Serial);
-          //   Serial.println();
-          // #elif TRANSMISSION_TYPE == WIFI_ONLY
-          //   serializeJson(doc, client); 
-          //   client.println();
-          // #elif TRANSMISSION_TYPE == WIFI_AND_SERIAL
-          //   serializeJson(doc, Serial);
-          //   Serial.println();
-          //   serializeJson(doc, client);
-          //   client.println();
-          // #endif
+          #if TRANSMISSION_TYPE == SERIAL_ONLY || TRANSMISSION_TYPE == WIFI_AND_SERIAL
+          serializeJson(doc, Serial);
+          Serial.println();
+          #endif
+          #if TRANSMISSION_TYPE == WIFI_ONLY || TRANSMISSION_TYPE == WIFI_AND_SERIAL
+          serializeJson(doc, client);
+          client.println();
+          #endif
           
+          // ang.clear();
+          d1.clear();
+          packet_counter = 0;
+        }
+
+        if(raw_data == 0xA0){
+
           buffer[0] = 0XFA;
           buffer[1] = raw_data;
           count = 2;
+          packet_counter = 0;
+          
           return;
         }
-    
-        if(count <= 22){
-          buffer[count] = raw_data;
-          count++;
-        }
+
+        buffer[0] = 0XFA;
+        buffer[1] = raw_data;
+
+        count = 2;
+        return;
       }
+
+      if(count <= 22){
+        buffer[count] = raw_data;
+        count++;
+      }
+    }
     
   #if TRANSMISSION_TYPE == WIFI_ONLY || TRANSMISSION_TYPE == WIFI_AND_SERIAL
   }else{
