75eeafc62dc3a2d0337b731b0059bc937a04c830
Yedi278 <Yehan278@gmail.com>
2025-03-06 01:34:26 +0100

    .

--- launch.bash
+++ launch.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MODEL=/home/utonto/ros2_ws/src/robot_slam/description/robot.urdf.xacro
-RVIZ=/home/utonto/ros2_ws/src/robot_slam/rviz/urdf.rviz
+MODEL=/mnt/c/Users/yehan/Documents/robot_slam/description/robot.urdf.xacro
+RVIZ=/mnt/c/Users/yehan/Documents/robot_slam/rviz/urdf.rviz
 
 ros2 launch robot_slam talker.launch.py model:=$MODEL rvizconfig:=$RVIZ
