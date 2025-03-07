a81d73a5bb8d90f262125e4f853218b42a085c40
Edi <yehan278@gmail.com>
2025-03-08 00:44:02 +0100

    Delete launch.bash

--- launch.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-MODEL=/home/utonto/ros2_ws/src/robot_slam/description/robot.urdf.xacro
-RVIZ=/home/utonto/ros2_ws/src/robot_slam/rviz/urdf.rviz
-
-ros2 launch robot_slam talker.launch.py model:=$MODEL rvizconfig:=$RVIZ
