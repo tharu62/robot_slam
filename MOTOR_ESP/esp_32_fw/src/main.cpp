017b8559e8595872d989857406721364c13cea70
Yedi278 <Yehan278@gmail.com>
2025-03-08 18:00:27 +0100

    motor encoder pid

--- MOTOR_ESP/esp_32_fw/src/main.cpp
+++ MOTOR_ESP/esp_32_fw/src/main.cpp
@@ -11,6 +11,7 @@
 #include <Arduino.h>
 #include <ArduinoJson.h>
 #include <HardwareSerial.h>
+#include <PIDController.h>
 #include <WiFi.h>
 #include <vector>
 #include <string>
@@ -32,7 +33,7 @@ WiFiClient client;
 extern const char* ssid;
 extern const char* password;
 
-int expected = 7000;
+long int expected = 7000;
 
 float kp=0.1;
 float ki=0.01;
@@ -44,29 +45,27 @@ int derivative=0.0;
 float error=0.0;
 float last_error=0.0;
 
-void periodic_counter_print(void *pvParameters){
-  while(1){
-    Serial.println(m1_count);
-    delay(100);
-
-  }
-}
-
 void pid_func(void *pvParameters) {
-
-  error = (float)(expected - m1_count);
-  proportional = kp * error;
-  integral += ki * error;
-  derivative = kd * (error - last_error);
-
-  float pid = proportional + integral + derivative;
-  last_error = error;
-
-  doc["d2"] = (pid > 0) - (pid < 0); // get sign of pid
-  doc["s2"] = abs((int)pid);
-  serializeJson(doc, arduino_serial);
-  Serial.printf("PID: %d",pid);
-  delay(200);
+  while(1){
+    error = (float)(expected - m1_count);
+    
+    proportional = kp * error;
+    // integral += ki * error;
+    derivative = kd * (error - last_error);
+    
+    float pid = proportional + integral + derivative;
+    last_error = error;
+    
+    doc["d1"] = (pid > 0) - (pid < 0); // get sign of pid
+    doc["s1"] = abs((int)pid);
+    serializeJson(doc, arduino_serial);
+    
+    Serial.println("Counter: " + String(m1_count));
+    Serial.printf("Error: %f, p:%f, i:%f, d:%f",error, proportional, integral, derivative);
+    Serial.printf("PID: %f\n",doc["s1"].as<float>());
+    
+    delay(200);
+  }
 }
 
 void setup() {
@@ -99,41 +98,20 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(M1_C1_PIN), interrupt_m1_c1, RISING);
   attachInterrupt(digitalPinToInterrupt(M1_C2_PIN), interrupt_m1_c2, RISING);
 
-  
-  auto a = xTaskCreate(periodic_counter_print, "periodic_counter_print", 10000, NULL, 1, NULL);
-  if(a == pdPASS){
-    Serial.println("Periodic Print created successfully");
+  // pid task
+  auto b = xTaskCreate(pid_func, "pid_func", 10000, NULL, 1, NULL);
+  if(b == pdPASS){
+    Serial.println("PID Task created successfully");
   }else{
-    Serial.println("Periodic Print creation failed");
+    Serial.println("PID Task creation failed");
   }
-
-  // auto b = xTaskCreate(pid_func, "pid_func", 10000, NULL, 1, NULL);
-  // if(b == pdPASS){
-  //   Serial.println("PID Task created successfully");
-  // }else{
-  //   Serial.println("PID Task creation failed");
-  // }
 
   m1_count = 0;
 }
 
 void loop() {
-
   if(arduino_serial.available()){
     String data = arduino_serial.readString();
     Serial.println(data);
   }
-
-  doc["d1"] = 1;
-  doc["s1"] = 100;
-  serializeJson(doc, arduino_serial);
-  Serial.println("Sent data to arduino");
-  serializeJson(doc, Serial);
-  Serial.println();
-
-  delay(1000);
-  doc["d1"] = 0;
-  doc["s1"] = 0;
-  serializeJson(doc, arduino_serial);
-  delay(1000);
 }
