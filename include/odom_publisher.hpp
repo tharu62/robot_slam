137a792a7ef732d7722fe0d2563111d99b7265ab
tharu62 <tharinduxdeshan@gmail.com>
2025-09-18 19:10:44 +0200

    first functioning iteration

--- include/odom_publisher.hpp
+++ include/odom_publisher.hpp
@@ -16,11 +16,13 @@
 #include "tf2_geometry_msgs/tf2_geometry_msgs.hpp" 
 #include "tf2_ros/static_transform_broadcaster.h"
 #include <tf2_ros/transform_broadcaster.h>
+#include "my_robot_interfaces/msg/dif_drive.hpp"
 
 #define BUFF_SIZE_1 50
 #define RADIUS 0.03
 #define WHEELBASE 0.2
 #define REDUCTION_RATIO 5940.0
+#define MAX_STEPS 11
 
 using namespace std::chrono_literals;
 
@@ -30,7 +32,7 @@ extern int client;
  * @brief This is a class that takes as input the values of revolution [N_left] [N_right] of the encoder 
  * on the wheels, the delta time [delta_time] necessary to make the revolution and produces as output 
  * the odometry message of a differential drive robot with the specified parameters defined above.
- * @note [Input] is receved by ros2 topic <wheel_steps>. 
+ * @note [Input] is receved by ros2 topic <dif_drive>. 
  * @note [Output] is published on the /odom topic.
  */
 class Odom_Publisher : public rclcpp::Node
@@ -38,8 +40,6 @@ class Odom_Publisher : public rclcpp::Node
   private:
   double new_x = 0.0;
   float angle = 0.0;
-  float N_left = 0.0;
-  float N_right = 0.0;
   float D_left = 0.0;
   float D_right = 0.0;
   float D_avg = 0.0;
@@ -57,22 +57,26 @@ class Odom_Publisher : public rclcpp::Node
   Odom_Publisher()
     : Node("odom_publisher")
     {
-
       tf_broadcaster_ = std::make_shared<tf2_ros::TransformBroadcaster>(this);
-      publisher_  = this->create_publisher<nav_msgs::msg::Odometry>("odom", 20);
-      timer_      = this->create_wall_timer( 0ms, std::bind(&Odom_Publisher::call_back, this));
+      publisher_  = this->create_publisher<nav_msgs::msg::Odometry>("odom", 10);
+      subscription_ = this->create_subscription<my_robot_interfaces::msg::DifDrive>("dif_drive", 10, std::bind(&Odom_Publisher::call_back, this, std::placeholders::_1));
 
     }
 
-  void call_back(){
+  void call_back(my_robot_interfaces::msg::DifDrive::SharedPtr msg){
+  // void call_back(){
+
+    // read wheel steps from the topic
+    delta_time = msg->dt;
 
     // Create and populate the Odometry message
     odom_msg.header.stamp = this->get_clock()->now();
     odom_msg.header.frame_id = "odom";
     odom_msg.child_frame_id = "base_link";
+
     // Oodometry calculation
-    D_left = N_left*2*M_PI*RADIUS;
-    D_right = N_right*2*M_PI*RADIUS;
+    D_left = msg->left_wheel_steps*2*M_PI*RADIUS/MAX_STEPS;
+    D_right = msg->right_wheel_steps*2*M_PI*RADIUS/MAX_STEPS;
     D_avg = (D_left + D_right)/2;
     delta_theta = (D_right - D_left)/WHEELBASE;
     tf2::Quaternion q;
@@ -87,6 +91,7 @@ class Odom_Publisher : public rclcpp::Node
     y = odom_msg.pose.pose.position.y;
     odom_msg.twist.twist.linear.x = D_avg/delta_time;
     odom_msg.twist.twist.angular.z = delta_theta/delta_time;
+
     // Publish the odometry message
     this->publisher_->publish(odom_msg);
 
@@ -95,11 +100,13 @@ class Odom_Publisher : public rclcpp::Node
     transform.header.stamp = this->get_clock()->now();
     transform.header.frame_id = "odom";
     transform.child_frame_id = "base_link";
+
     // Here we use the same dummy data as the odometry message
     transform.transform.translation.x = odom_msg.pose.pose.position.x;
     transform.transform.translation.y = odom_msg.pose.pose.position.y;
     transform.transform.translation.z = 0.0;
     transform.transform.rotation = odom_msg.pose.pose.orientation;    
+
     // Broadcast the transform
     tf_broadcaster_->sendTransform(transform);
 
@@ -109,6 +116,7 @@ class Odom_Publisher : public rclcpp::Node
   rclcpp::TimerBase::SharedPtr timer_;
   std::shared_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
   rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr publisher_;
+  rclcpp::Subscription<my_robot_interfaces::msg::DifDrive>::SharedPtr subscription_;
   
 };
 
